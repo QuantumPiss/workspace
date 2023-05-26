4dca00cfe78d5e75cf9469dea111292a08b84a28
QuantumPiss <annieethegoat@gmail.com>
2023-05-25 21:00:21 -0400

    finished chapter 3

--- learn_django/django_pt1/chapter_3/django_project/urls.py
+++ learn_django/django_pt1/chapter_3/django_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
-    path("about/", include("pages.urls")),
+    path("others/", include("otherpages.urls")),
 ]
--- learn_django/django_pt1/chapter_3/pages/urls.py
+++ learn_django/django_pt1/chapter_3/pages/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import AboutPageView
 from .views import HomePageView
+from .views import ContactPageView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path("contact/", ContactPageView.as_view(), name="contact"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("", HomePageView.as_view(), name="home"),
 ]
